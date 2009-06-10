4593226f787dbe431f124d3364ce49c6460fcd9e
Albrecht Gebhardt <albrecht.gebhardt@uni-klu.ac.at>
2009-06-10 00:00:00 +0000

    version 0.5-3

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .First.lib <- function(lib, pkg) {
-  if(version$major==0 && version$minor < 62)
+  if(getRversion()<="0.62")
     stop("This version for R 0.62 or later")
   library.dynam("akima", pkg, lib)
 }
