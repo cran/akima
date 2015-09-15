a106c69a273de81d435c7b65a256d8f02cb1677b
Albrecht Gebhardt <albrecht.gebhardt@aau.at>
2015-09-15 15:15:20 +0000

    version 0.5-12

--- R/interpp.R
+++ R/interpp.R
@@ -1,4 +1,4 @@
-"interpp"<-function(x, y, z, xo, yo, linear = TRUE, extrap = FALSE,
+"interpp"<-function(x, y=NULL, z, xo, yo=NULL, linear = TRUE, extrap = FALSE,
                     duplicate = "error", dupfun = NULL, ncp=NULL)
 {
 
@@ -12,11 +12,43 @@
     else
       stop('ncp < 0 ?')
   }
-  
+  ## handle sp data, save coordinate and value names
+  is.sp <- FALSE
+  sp.coord <- NULL
+  sp.z <- NULL
+  sp.proj4string <- NULL
+  if(is.null(y)&&is.character(z)){
+      if(class(xo)=="SpatialPointsDataFrame"){
+          yo <- coordinates(xo)[,2]
+          xo <- coordinates(xo)[,1]
+      } else
+          stop("either x,y,z,xo,yo have to be numeric vectors or
+both x and xo have to be SpatialPointsDataFrames and z a name of a data column in x")
+      if(class(x)=="SpatialPointsDataFrame"){
+          sp.coord <- dimnames(coordinates(x))[[2]]
+          sp.z <- z
+          sp.proj4string <- x@proj4string
+          z <- x@data[,z]
+          y <- coordinates(x)[,2]
+          x <- coordinates(x)[,1]
+          is.sp <- TRUE
+      } else
+          stop("either x,y,z,xo,yo have to be numeric vectors or
+both x and xo have to be SpatialPointsDataFrames and z a name of a data column in x")
+  }
+
   if(linear)
-                                        # the old Akima code:
-    interpp.old(x, y, z, xo, yo, ncp=0, extrap, duplicate, dupfun)
+      ## the old Akima code:
+      ret <- interpp.old(x, y, z, xo, yo, ncp=0, extrap, duplicate, dupfun)
   else
-                                        # new code for splines
-    interpp.new(x, y, z, xo, yo, extrap, duplicate, dupfun)
+      ## new code for splines
+      ret <- interpp.new(x, y, z, xo, yo, extrap, duplicate, dupfun)
+  if(is.sp){
+      nona <- !is.na(ret$z)
+      ret <- data.frame(ret$x[nona],ret$y[nona],ret$z[nona])
+      names(ret) <- c(sp.coord[1],sp.coord[2],sp.z)
+      coordinates(ret) <- sp.coord
+      ret@proj4string <- sp.proj4string
+  }
+  ret
 }
